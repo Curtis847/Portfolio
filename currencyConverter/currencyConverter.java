4a3807cdacc96dba377002754b186391f5910337
Curtis847 <murphy.curtis12@gmail.com>
2020-02-23 20:33:17 +0000

    Second commit. Added other Scanner functionality

--- currencyConverter/currencyConverter.java
+++ currencyConverter/currencyConverter.java
@@ -4,7 +4,7 @@ import java.text.DecimalFormat;
 public class currencyConverter {
   public static void main (String[] args) {
     double amount, pound, usd, euro;
-    String choice;
+    String choice, choiceTwo;
 
     DecimalFormat format = new DecimalFormat("##.##");
 
@@ -15,22 +15,62 @@ public class currencyConverter {
     choice = scan.nextLine();
 
     if (choice.equals("euro")) {
-      System.out.println("Please enter the amount you wish to convert: ");
-      amount = scan.nextFloat();
-      pound = amount * 0.84;
-      System.out.println("Your " + amount + " euros is " + format.format(pound) + " pounds.");
+      System.out.println("Please enter the currency you wish to convert to: ");
+      choiceTwo = scan.nextLine();
+      if(choiceTwo.equals("pound")) {
+        System.out.println("Please enter the amount you wish to convert: ");
+        amount = scan.nextFloat();
+        pound = amount * 0.84;
+        System.out.println("Your " + amount + " euros is " + format.format(pound) + " pounds.");
+      }
+      else if (choiceTwo.equals("usd")) {
+        System.out.println("Please enter the amount you wish to convert: ");
+        amount = scan.nextFloat();
+        usd = amount * 1.08;
+        System.out.println("Your " + amount + " euros is " + format.format(usd) + " dollars.");
+      }
+      else {
+        System.out.println("Invalid!");
+      }
+
     }
     else if (choice.equals("pound")) {
-      System.out.println("Please enter the amount you wish to convert: ");
-      amount = scan.nextFloat();
-      usd = amount * 1.29;
-      System.out.println("Your " + amount + " pounds is " + format.format(usd) + " dollars.");
+      System.out.println("Please enter the currency you wish to convert to: ");
+      choiceTwo = scan.nextLine();
+      if(choiceTwo.equals("usd")) {
+        System.out.println("Please enter the amount you wish to convert: ");
+        amount = scan.nextFloat();
+        usd = amount * 1.29;
+        System.out.println("Your " + amount + " pounds is " + format.format(usd) + " dollars.");
+      }
+      else if (choiceTwo.equals("euro")) {
+        System.out.println("Please enter the amount you wish to convert: ");
+        amount = scan.nextFloat();
+        euro = amount * 1.20;
+        System.out.println("Your " + amount + " pounds is " + format.format(euro) + " euros.");
+      }
+      else {
+        System.out.println("Invalid!");
+      }
     }
     else if (choice.equals("usd")) {
-      System.out.println("Please enter the amount you wish to convert: ");
-      amount = scan.nextFloat();
-      euro = amount * 0.93;
-      System.out.println("Your " + amount + " dollars is " + euro + " euros.");
+      System.out.println("Please enter the currency you wish to convert to: ");
+      choiceTwo = scan.nextLine();
+      if(choiceTwo.equals("euro")) {
+        System.out.println("Please enter the amount you wish to convert: ");
+        amount = scan.nextFloat();
+        euro = amount * 0.93;
+        System.out.println("Your " + amount + " dollars is " + euro + " euros.");
+      }
+      else if (choiceTwo.equals("pound")) {
+        System.out.println("Please enter the amount you wish to convert: ");
+        amount = scan.nextFloat();
+        pound = amount * 0.77;
+        System.out.println("Your " + amount + " dollars is " + pound + " pounds.");
+      }
+      else {
+        System.out.println("Invalid!");
+      }
     }
     else {
       System.out.println("Invalid!");
